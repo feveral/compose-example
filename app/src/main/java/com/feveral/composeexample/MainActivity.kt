be66629394ac1077a2c51dba9d6ed16b263861a8
楊宗翰 <feveraly@gmail.com>
2023-02-19 17:15:36 +0800

    feat: make meme view prettier

--- app/src/main/java/com/feveral/composeexample/MainActivity.kt
+++ app/src/main/java/com/feveral/composeexample/MainActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil.compose.AsyncImage
@@ -56,11 +58,20 @@ class MainActivity : ComponentActivity() {
 
         LazyColumn(modifier = Modifier.fillMaxHeight()) {
             items(vm.memeList) { meme ->
-                Column {
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(1.dp, Color.Gray)
+                        .padding(16.dp)
+                ) {
                     Row {
                         AsyncImage(
                             model = meme.user.avatarUrl,
-                            contentDescription = null
+                            contentDescription = null,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(40.dp)
+                                .padding(5.dp)
                         )
                         Text(
                             meme.user.name,
@@ -68,15 +79,21 @@ class MainActivity : ComponentActivity() {
                             fontWeight = FontWeight.Bold
                         )
                     }
-                    AsyncImage(
-                        model = meme.image.url,
-                        contentDescription = null
-                    )
                     Text(
                         meme.description,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        AsyncImage(
+                            model = meme.image.url,
+                            contentDescription = null,
+                        )
+                    }
                 }
             }
         }
